f161fa5bb93f5d709e5eaca9339c4ca5e24c4427
미르 <alfmalfmal2@gmail.com>
2020-07-16 20:30:57 +0900

    Updates

---
+++ precache-manifest.03c6ac8ca8bb5a094149485e4cd629c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3672906f75d9353941cb4b2d25534a71",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "10ff2679331aae10ba1b",
+    "url": "/movie_app/static/css/main.5721af45.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10ff2679331aae10ba1b",
+    "url": "/movie_app/static/js/main.a94af76c.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
